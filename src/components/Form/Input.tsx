99a6d1bc7e8f415d03337ecd500b75183d65abcb
Guilherme loli <guilherme.loli@brtk.com.br>
2022-08-04 21:51:57 -0300

    config project, dashboard development and some components

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,28 @@
+import {
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+} from '@chakra-ui/react';
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+  type: string;
+}
+
+export function Input({ name, label, ...rest }: InputProps) {
+  return (
+    <ChakraInput
+      name={name}
+      id={name}
+      focusBorderColor='pink.500'
+      bgColor='gray.900'
+      variant='filled'
+      _hover={{
+        bgColor: 'gray.900',
+      }}
+      size='lg'
+      placeholder={label}
+      {...rest}
+    />
+  );
+}
